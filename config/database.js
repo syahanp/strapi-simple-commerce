a3a3024611db6f83861dbfe2b48a5c74f3b1b4d3
Aligar Syahan Putra <aligar.gligar@gmail.com>
2021-02-26 17:55:21 +0700

    db

--- config/database.js
+++ config/database.js
@@ -4,17 +4,32 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        host: env('DATABASE_HOST', 'cluster0.75rpu.mongodb.net'),
-        srv: env.bool('DATABASE_SRV', true),
-        port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'strapi-simple-commerce'),
-        username: env('DATABASE_USERNAME', 'syahanp'),
-        password: env('DATABASE_PASSWORD', 'G0Rum57WTA7CigHi'),
+        uri: env('DATABASE_URI'),
       },
       options: {
-        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-        ssl: env.bool('DATABASE_SSL', true),
+        ssl: true,
       },
     },
   },
 });
+
+// module.exports = ({ env }) => ({
+//   defaultConnection: 'default',
+//   connections: {
+//     default: {
+//       connector: 'mongoose',
+//       settings: {
+//         host: env('DATABASE_HOST', 'cluster0.75rpu.mongodb.net'),
+//         srv: env.bool('DATABASE_SRV', true),
+//         port: env.int('DATABASE_PORT', 27017),
+//         database: env('DATABASE_NAME', 'strapi-simple-commerce'),
+//         username: env('DATABASE_USERNAME', 'syahanp'),
+//         password: env('DATABASE_PASSWORD', 'G0Rum57WTA7CigHi'),
+//       },
+//       options: {
+//         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+//         ssl: env.bool('DATABASE_SSL', true),
+//       },
+//     },
+//   },
+// });
