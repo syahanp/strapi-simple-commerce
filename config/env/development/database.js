6edc00ff67ebeac2e94d4ca5d9e427680755c4db
Aligar Syahan Putra <aligar.gligar@gmail.com>
2021-02-26 18:04:11 +0700

    update

---
+++ config/env/development/database.js
@@ -0,0 +1,20 @@
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'mongoose',
+      settings: {
+        host: env('DATABASE_HOST', 'cluster0.75rpu.mongodb.net'),
+        srv: env.bool('DATABASE_SRV', true),
+        port: env.int('DATABASE_PORT', 27017),
+        database: env('DATABASE_NAME', 'strapi-simple-commerce'),
+        username: env('DATABASE_USERNAME', 'syahanp'),
+        password: env('DATABASE_PASSWORD', 'G0Rum57WTA7CigHi'),
+      },
+      options: {
+        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+        ssl: env.bool('DATABASE_SSL', true),
+      },
+    },
+  },
+});
